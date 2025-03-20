2a1a00590f58b84fcba733f1aa11dda6d0bacd94
emzilia <em@zilia.dev>
2025-03-20 18:02:07 -0400

    swapped bar positions

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,13 +5,13 @@ killall -q polybar
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar left &
-polybar center &
-polybar playerctl &
-polybar right &
 polybar left2 &
 polybar center2 &
 polybar playerctl2 &
 polybar right2 &
+polybar left &
+polybar center &
+polybar playerctl &
+polybar right &
 
 echo "Polybar launched..."
