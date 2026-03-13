4510a498c10e2cdeeca225240a7da2eaa3a3dbcd
emzilia <em@zilia.dev>
2026-03-13 18:15:04 -0400

    added mkdir -p for relevant dirs, just in case

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Sets up symbolic links to all relevant dotfiles
 
+mkdir -p "$HOME"/.bashrc.d
+
 ln -sf "$HOME"/.dotfiles/.bashrc \
        "$HOME"/.bashrc
-
-mkdir "$HOME"/.bashrc.d
 
 ln -sf "$HOME"/.dotfiles/.bashrc.d/bash_aliases \
        "$HOME"/.bashrc.d/bash_aliases
@@ -20,6 +20,11 @@ ln -sf "$HOME"/.dotfiles/.writerc \
        "$HOME"/.writerc
 
 # Wayland - Sway
+
+mkdir -p "$HOME"/.config/sway
+mkdir -p "$HOME"/.config/waybar
+mkdir -p "$HOME"/.config/yofi
+
 ln -sf "$HOME"/.dotfiles/.config/sway/config \
        "$HOME"/.config/sway/config
 
@@ -33,6 +38,11 @@ ln -sf "$HOME"/.dotfiles/.config/waybar/style.css \
        "$HOME"/.config/waybar/style.css
 
 # X11 - i3
+
+mkdir -p "$HOME"/.config/i3
+mkdir -p "$HOME"/.config/polybar
+mkdir -p "$HOME"/.config/rofi
+
 ln -sf "$HOME"/.dotfiles/.config/i3/config \
        "$HOME"/.config/i3/config
 
@@ -47,5 +57,4 @@ ln -sf "$HOME"/.dotfiles/.config/polybar/config.ini \
 
 ln -sf "$HOME"/.dotfiles/.config/polybar/launch.sh \
        "$HOME"/.config/polybar/launch.sh
-
 
